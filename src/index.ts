abc0a456d0e5548c4fe61b3e7a69fabd52d28a26
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-10-10 10:12:42 +0700

    Improve type documentation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,121 @@
 import pako from 'pako';
 import { Buffer } from 'buffer';
 
+
+// #region Types
+/** Represents the decompressed replay data as stored in-game. */
+export type GameReplayData = {
+    /**
+     * A list of input events that occurred during the replay.  
+     * Note: does not exist in the raw game metadata.
+     */
+    inputs: GameInputEvent[];
+    
+    /** Whether or not the replay is marked as a TAS. */
+    tasUsed?: boolean;
+
+    /** Unknown, but set to false. */
+    private?: boolean;
+
+    /** The username of the player. */
+    player: string;
+
+    /** The seed for the random number generator. */
+    seed: number;
+
+    /** The version of the game the replay was made in. */
+    version: string;
+
+    /** The time the replay was initially created. */
+    date: string;
+
+    /**
+     * A list of mods applied to the run.  
+     * It's in the format of [mod, value], where mod is the mod ID and value is the value of the mod.  
+     */
+    mod: [number, number][];
+
+    /** The name of the mode that was played. */
+    mode: string;
+
+    /** The settings of the game when the run was played. */
+    setting: {
+        shakeFX?: number;
+        splashFX?: number;
+        das?: number;
+        highCam?: boolean;
+        smooth?: boolean;
+        warn?: boolean;
+        dropcut?: number;
+        ghost?: number;
+        atkFX?: number;
+        nextPos?: boolean;
+        block?: boolean;
+        text?: boolean;
+        ihs?: boolean;
+        face?: number[];
+        score?: boolean;
+        irs?: boolean;
+        center?: number;
+        sdarr?: number;
+        moveFX?: number;
+        dropFX?: number;
+        ims?: boolean;
+        lockFX?: number;
+        arr?: number;
+        swap?: boolean;
+        bagLine?: boolean;
+        skin?: number[];
+        grid?: number;
+        dascut?: number;
+        sddas?: number;
+        RS?: string;
+        clearFX?: number;
+        [key: string]: unknown;
+    };
+
+    /** Additional replay metadata that may not be standard. */
+    [key: string]: unknown;
+}
+
+/** Represents a single input event in a replay. */
+export type GameInputEvent = {
+    /** The frame the input event occured. */
+    frame: number;
+    /** The kind of input event (press or release). */
+    type: InputEventType;
+    /** The key that was pressed or released. */
+    key: InputKey;
+}
+
+/** Represents the kind of input event. */
+export const InputEventType = {
+    Press: 0, Release: 1
+} as const;
+
+export type InputEventType = typeof InputEventType[keyof typeof InputEventType];
+
+/** Represents the input button of an input event. */
+export const InputKey = {
+    Invalid: 0,
+
+    MoveLeft: 1, MoveRight: 2,
+    RotateRight: 3, RotateLeft: 4, Rotate180: 5,
+    HardDrop: 6, SoftDrop: 7,
+    Hold: 8,
+
+    Function1: 9, Function2: 10,
+    
+    InstantLeft: 11, InstantRight: 12,
+    SonicDrop: 13,
+    Down1: 14, Down4: 15, Down10: 16,
+    LeftDrop: 17, RightDrop: 18,
+    LeftZangi: 19, RightZangi: 20
+} as const;
+
+export type InputKey = typeof InputKey[keyof typeof InputKey];
+// #endregion
+
 function decodeVLQ(data: Uint8Array, position: number): [number, number] {
     let ret = 0;
     let byte = 0;
@@ -102,88 +217,3 @@ if(typeof window !== 'undefined') {
         parseReplayFromRepString
     }
 }
-
-// #region Types
-/** Represents the decompressed replay data as stored in-game. */
-export type GameReplayData = {
-    inputs: GameInputEvent[];
-    tasUsed?: boolean;
-    private?: boolean;
-    player: string;
-    seed: number;
-    version: string;
-    date: string;
-    mod: [number, number][]; // [mod, value]
-    mode: string;
-    setting: {
-        shakeFX?: number;
-        splashFX?: number;
-        das?: number;
-        highCam?: boolean;
-        smooth?: boolean;
-        warn?: boolean;
-        dropcut?: number;
-        ghost?: number;
-        atkFX?: number;
-        nextPos?: boolean;
-        block?: boolean;
-        text?: boolean;
-        ihs?: boolean;
-        face?: number[];
-        score?: boolean;
-        irs?: boolean;
-        center?: number;
-        sdarr?: number;
-        moveFX?: number;
-        dropFX?: number;
-        ims?: boolean;
-        lockFX?: number;
-        arr?: number;
-        swap?: boolean;
-        bagLine?: boolean;
-        skin?: number[];
-        grid?: number;
-        dascut?: number;
-        sddas?: number;
-        RS?: string;
-        clearFX?: number;
-        [key: string]: unknown;
-    };
-    [key: string]: unknown;
-}
-
-/** Represents a single input event in a replay. */
-export type GameInputEvent = {
-    frame: number;
-    type: InputEventType;
-    key: InputKey;
-}
-
-/** Represents the kind of input event. */
-export const InputEventType = {
-    Press: 0, Release: 1
-} as const;
-
-export type InputEventType = typeof InputEventType[keyof typeof InputEventType];
-
-/** Represents the input button of an input event. */
-export const InputKey = {
-    Invalid: 0,
-
-    MoveLeft: 1, MoveRight: 2,
-    RotateRight: 3, RotateLeft: 4, Rotate180: 5,
-    HardDrop: 6, SoftDrop: 7,
-    Hold: 8,
-
-    Function1: 9, Function2: 10,
-    
-    InstantLeft: 11, InstantRight: 12,
-    SonicDrop: 13,
-    Down1: 14, Down4: 15, Down10: 16,
-    LeftDrop: 17, RightDrop: 18,
-    LeftZangi: 19, RightZangi: 20
-} as const;
-
-export type InputKey = typeof InputKey[keyof typeof InputKey];
-
-// #endregion
